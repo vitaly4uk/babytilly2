a830d8b2e5c091603b6f9688070a53cd86118c05
vitalii.omelchuk <vitalii.omelchuk@ey.com>
2021-09-12 02:01:42 +0300

    static root

--- babytilly2/settings.py
+++ babytilly2/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
