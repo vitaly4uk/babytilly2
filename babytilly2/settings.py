18a8f403f6776b6b308aa3a2bf763a9504d49ec3
vitalii.omelchuk <vitalii.omelchuk@ey.com>
2021-09-12 12:13:09 +0300

    logging settings

--- babytilly2/settings.py
+++ babytilly2/settings.py
@@ -170,6 +170,51 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_QUERYSTRING_EXPIRE = None
 AWS_QUERYSTRING_AUTH = False
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'commercial': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
+}
+
 try:
     from local_settings import *
 except ImportError:
