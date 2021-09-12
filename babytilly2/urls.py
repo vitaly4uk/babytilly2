e61de814cf61bb107f0459ca680618f5360b5cf6
vitalii.omelchuk <vitalii.omelchuk@ey.com>
2021-09-12 11:35:38 +0300

    index page

--- babytilly2/urls.py
+++ babytilly2/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+from commercial.views import HomePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('search/', HomePage.as_view(), name='search'),
+    path('', HomePage.as_view(), name='home_page'),
+] + staticfiles_urlpatterns()
--- commercial/views.py
+++ commercial/views.py
@@ -1,3 +1,28 @@
+from django.core.files.storage import get_storage_class
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.core.cache import cache
 
-# Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePage, self).get_context_data()
+        file_list = cache.get('home-page-file-list')
+        if not file_list:
+            file_list = []
+            storage = get_storage_class()()
+            try:
+                dirs, files = storage.listdir('upload/startpage')
+            except FileNotFoundError:
+                files = []
+            for filename in files:
+                if filename == '.':
+                    continue
+                file_list.append(storage.url('{}/{}'.format('upload/startpage', filename)))
+            cache.set('home-page-file-list', file_list)
+        context.update({
+            'file_list': file_list,
+        })
+        return context
