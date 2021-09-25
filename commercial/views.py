04aa240b3def568ebce962da00ecd8f66934712c
Noel218 <nomax.com.ua@gmail.com>
2021-09-25 17:35:07 +0300

    index file_list from StartPageImage

--- commercial/views.py
+++ commercial/views.py
@@ -3,25 +3,31 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.cache import cache
 
+from commercial.models import StartPageImage
+
 
 class HomePage(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data()
-        file_list = cache.get('home-page-file-list')
-        if not file_list:
-            file_list = []
-            storage = get_storage_class()()
-            try:
-                dirs, files = storage.listdir('upload/startpage')
-            except Exception:
-                files = []
-            for filename in files:
-                if filename == '.':
-                    continue
-                file_list.append(storage.url('{}/{}'.format('upload/startpage', filename)))
-            cache.set('home-page-file-list', file_list)
+        if self.request.user.is_authenticated:
+            department = self.request.user.profile.department_id
+            file_list = cache.get(f'home-page-file-list-{department}')
+            if not file_list:
+                file_list = []
+                storage = get_storage_class()()
+                for image in StartPageImage.objects.filter(departament_id=department):
+                    file_list.append(storage.url(image.image.url))
+                cache.set(f'home-page-file-list-{department}', file_list)
+        else:
+            file_list = cache.get(f'home-page-file-list-free')
+            if not file_list:
+                file_list = []
+                storage = get_storage_class()()
+                for image in StartPageImage.objects.filter(departament__isnull=True):
+                    file_list.append(storage.url(image.image.url))
+                cache.set(f'home-page-file-list-free', file_list)
         context.update({
             'file_list': file_list,
         })
