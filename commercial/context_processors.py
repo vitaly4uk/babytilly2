8a0a1a78edb53766b57b9dbbbd40020d01ee1e56
vitaly4uk <vitaly.omelchuk@gmail.com>
2021-10-20 00:36:46 +0300

    optimize context processor

--- commercial/context_processors.py
+++ commercial/context_processors.py
@@ -1,10 +1,9 @@
-from django.core.cache import cache
-from commercial.models import Category, CategoryProperties
+from commercial.models import CategoryProperties
 
 
 def root_sections(request):
     categories = []
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and request.user.profile:
         categories = CategoryProperties.objects.filter(published=True, department__id=request.user.profile.department_id).select_related('category').order_by('category__tree_id', 'category__lft')
 
     return {
