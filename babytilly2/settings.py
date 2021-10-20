d4dd4c027380120086e32fb330e220120db5f7ed
vitaly4uk <vitaly.omelchuk@gmail.com>
2021-10-20 15:38:51 +0300

    sentry integration

--- babytilly2/settings.py
+++ babytilly2/settings.py
@@ -16,6 +16,23 @@ from urllib.parse import urlparse
 import dj_database_url
 from kombu.utils.url import safequote
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://cac659ce4a9244d19d651e905ab3d79f@o247047.ingest.sentry.io/6021103",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.1,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 EMAIL_BACKEND = 'django_ses.SESBackend'
 DEFAULT_FROM_EMAIL = 'tilly.zakaz@gmail.com'
 SERVER_EMAIL = 'tilly.zakaz@gmail.com'
